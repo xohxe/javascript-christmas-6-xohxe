aae412281c7d2a265c7e293e1b2a704085f6df59
Sohye <saeyan2477@gmail.com>
2023-11-12 21:43:29 +0900

    feat: Constants 추가
    - 메뉴 리스트와 가격 상수 추가

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,3 +1,29 @@
+const MENU_LIST = {
+  MENU_APPETIZER: {
+    양송이수프: 6000,
+    타파스: 5500,
+    시저샐러드: 8000,
+  },
+
+  MENU_MAIN: {
+    티본스테이크: 55000,
+    바비큐립: 54000,
+    해산물파스타: 35000,
+    크리스마스파스타: 25000,
+  },
+
+  MENU_DESSERT: {
+    초코케이크: 15000,
+    아이스크림: 5000,
+  },
+
+  MENU_BEVERAGE: {
+    제로콜라: 3000,
+    레드와인: 60000,
+    샴페인: 25000,
+  },
+};
+
 const EVENT_RULE = {
   RANGE: {
     MIN: 1,
@@ -16,9 +42,6 @@ const INPUT_MESSAGES = Object.freeze({
 const OUPUT_MESSAGES = Object.freeze({
   PREVIEW_INFO: '12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!',
   ORDER_MENU: '<주문 메뉴>',
-  menuList(menus) {
-    return `[${menus.join(', ')}]`;
-  },
   ORDER_AMOUNT_SUM: '<할인 전 총주문 금액>',
   GIFT_MENU: '<증정 메뉴>',
   BENEFITS_DETAILS: '<혜택 내역>',
@@ -33,4 +56,10 @@ const ERROR_MESSAGES = Object.freeze({
   NO_MENU: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
   NO_VALUE: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
 });
-export { EVENT_RULE, INPUT_MESSAGES, OUPUT_MESSAGES, ERROR_MESSAGES };
+export {
+  MENU_LIST,
+  EVENT_RULE,
+  INPUT_MESSAGES,
+  OUPUT_MESSAGES,
+  ERROR_MESSAGES,
+};
