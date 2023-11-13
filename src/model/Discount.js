d03ba178d4da432dc537cf2e706aa0753fef261a
Sohye <saeyan2477@gmail.com>
2023-11-13 23:43:16 +0900

    feat: Discount 클래스 구현
    - 날짜와 주문내역을 넘겨받아 케이스에 맞게 이벤트를 적용하여 할인금액을 계산하는 함수를 구현하였다.
    - 디데이할인, 주중할인, 주말할인, 특별할인, 증정이벤트 함수 구현하였다.
    - 적용받은 할인 금액을 reduce 함수를 이용하여 합산한다.
    - 할인금액을 모두 discountList 배열로 저장한다.

---
+++ src/model/Discount.js
@@ -0,0 +1,84 @@
+import { DAYS, MENU_LIST } from '../utils/Constants';
+
+class Discount {
+  constructor() {}
+
+  checkDiscountList(date, menuList) {
+    let discountList = [];
+    discountList.push(this.dDayDiscount(date));
+    discountList.push(this.weekdayDiscount(date, menuList));
+    discountList.push(this.weekendDiscount(date, menuList));
+    discountList.push(this.specialDiscount(date));
+    discountList.push(this.giftEvent[1]);
+    return discountList;
+  }
+  // 총 혜택 금액
+  calcSumDiscount(discountList) {
+    let allDiscount = discountList.filter((element) => element !== undefined);
+    const sumOfDiscount = allDiscount.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+    return sumOfDiscount;
+  }
+
+  // 디데이 할인
+  dDayDiscount(date) {
+    let discount = 0;
+    if (date < 26) {
+      discount = 1000 + date * 100;
+    }
+    return discount;
+  }
+
+  // 평일 할인
+  weekdayDiscount(date, menuList) {
+    const WEEKDAYS = DAYS.makeWeekdays();
+    const validWeekday = menuList.map((menu) => {
+      if (
+        WEEKDAYS.includes(date) &&
+        Object.keys(MENU_LIST.MENU_DESSERT).includes(menu.name)
+      ) {
+        return menu.amount;
+      }
+    });
+    let newValidWeekday = validWeekday.filter((el) => el !== undefined);
+    const sumOfDiscount = newValidWeekday.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+    return 2023 * sumOfDiscount;
+  }
+  // 주말 할인
+  weekendDiscount(date, menuList) {
+    const WEEKENDS = DAYS.makeWeekends();
+    const validWeekend = menuList.map((menu) => {
+      if (
+        WEEKENDS.includes(date) &&
+        Object.keys(MENU_LIST.MENU_MAIN).includes(menu.name)
+      ) {
+        return menu.amount;
+      }
+    });
+    let newValidWeekend = validWeekend.filter((el) => el !== undefined);
+    const sumOfDiscount = newValidWeekend.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+    return 2023 * sumOfDiscount;
+  }
+
+  // 특별 할인
+  specialDiscount(date) {
+    const STARDAYS = DAYS.makeStarDays();
+    if (STARDAYS.includes(date)) {
+      return 1000;
+    }
+  }
+  // 증정 메뉴
+  giftEvent(price) {
+    let gift = ['없음', 0];
+    if (price >= 120000) {
+      gift = ['샴페인', 25000];
+    }
+    return gift;
+  }
+}
+export default Discount;
