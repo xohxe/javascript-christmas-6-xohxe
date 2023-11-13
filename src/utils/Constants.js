c9d7dd5be2042ae4c9a925e5949b15c852d62a21
Sohye <saeyan2477@gmail.com>
2023-11-13 23:19:26 +0900

    feat: Constants 내 메뉴 리스트, 날짜 계산 메서드, 이벤트 이름 및 뱃지 이름 추가
    - `ALL_MENU_LIST` : Object.assign을 이용하여 메뉴리스트 객체를 병합
    - `DAYS`: 주중, 주말, 특별할인 날짜 주기를 계산하는 함수 추가
    - `EVENT_NAME`, `EVENT_BADGE` : 전역으로 사용되는 이벤트 이름, 뱃지 이름 상수 추가
    - 변경한 상수명 변경하여 OuputView에 적용

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -24,6 +24,64 @@ const MENU_LIST = {
   },
 };
 
+const ALL_MENU_LIST = Object.assign(
+  {},
+  MENU_LIST.MENU_APPETIZER,
+  MENU_LIST.MENU_MAIN,
+  MENU_LIST.MENU_DESSERT,
+  MENU_LIST.MENU_BEVERAGE,
+);
+
+const DAYS = {
+  makeWeekdays() {
+    let WEEKDAY = [4, 5, 6, 7];
+    let WEEKDAYS = [];
+    for (let i = 0; i < 4; i++) {
+      WEEKDAY.map((e) => {
+        WEEKDAYS.push(e + 7 * i);
+      });
+    }
+    return WEEKDAYS;
+  },
+
+  makeWeekends() {
+    let WEEKEND = [1, 2];
+    let WEEKENDS = [];
+    for (let i = 0; i < 5; i++) {
+      WEEKEND.map((e) => {
+        WEEKENDS.push(e + 7 * i);
+      });
+    }
+    return WEEKENDS;
+  },
+
+  makeStarDays() {
+    let STARDAY = [3];
+    let STARDAYS = [];
+    for (let i = 0; i < 5; i++) {
+      STARDAY.map((e) => {
+        STARDAYS.push(e + 7 * i);
+      });
+    }
+    STARDAYS.push(25);
+    return STARDAYS;
+  },
+};
+
+const EVENT_NAME = [
+  '크리스마스 디데이 할인:',
+  '평일 할인:',
+  '주말 할인:',
+  '특별 할인:',
+  '증정 이벤트:',
+];
+
+const EVENT_BADGE = {
+  5000: '별',
+  10000: '트리',
+  20000: '산타',
+};
+
 const EVENT_RULE = {
   RANGE: {
     MIN: 1,
@@ -42,11 +100,11 @@ const INPUT_MESSAGES = Object.freeze({
 const OUPUT_MESSAGES = Object.freeze({
   PREVIEW_INFO: '12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!',
   ORDER_MENU: '<주문 메뉴>',
-  ORDER_AMOUNT_SUM: '<할인 전 총주문 금액>',
+  ORDER_PRICE: '<할인 전 총주문 금액>',
   GIFT_MENU: '<증정 메뉴>',
   BENEFITS_DETAILS: '<혜택 내역>',
   TOTAL_BENEFIT_AMOUNT: '<총혜택 금액>',
-  DISCOUNT_AMOUNT: '<할인 후 예상 결제 금액>',
+  DISCOUNT_PRICE: '<할인 후 예상 결제 금액>',
   EVENT_BADGE: '<12월 이벤트 배지>',
 });
 
@@ -56,8 +114,13 @@ const ERROR_MESSAGES = Object.freeze({
   NO_ORDER: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
   NO_VALUE: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
 });
+
 export {
   MENU_LIST,
+  ALL_MENU_LIST,
+  DAYS,
+  EVENT_NAME,
+  EVENT_BADGE,
   EVENT_RULE,
   INPUT_MESSAGES,
   OUPUT_MESSAGES,
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,14 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
-import { OUPUT_MESSAGES } from '../utils/Constants.js';
-import Utils from '../utils/Util.js';
+import { OUPUT_MESSAGES, EVENT_NAME } from '../utils/Constants.js';
 
 const OutputView = {
-    
-  // START ~ <주문 메뉴> 출력
+  // START
   printIntro() {
     Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
-
+  // <주문 메뉴> 출력
   printMenu(menus) {
     Console.print(OUPUT_MESSAGES.ORDER_MENU);
     menus.forEach((menu) => {
@@ -17,28 +15,43 @@ const OutputView = {
   },
 
   // <할인 전 총주문 금액> 출력
-  printOrderAmount() {
-    Console.print(OUPUT_MESSAGES.ORDER_AMOUNT_SUM);
+  printOrderPrice(price) {
+    Console.print(
+      `\n${OUPUT_MESSAGES.ORDER_PRICE}\n${price.toLocaleString()}원`,
+    );
+  },
+
+  // <증정 메뉴> 출력
+  printGift(gift) {
+    Console.print(`\n${OUPUT_MESSAGES.GIFT_MENU}\n${gift[0]}`);
   },
 
   // <혜택 내역> 출력
-  printBenefit() {
-    Console.print(OUPUT_MESSAGES.BENEFITS_DETAILS);
+  printBenefit(discountList) {
+    Console.print(`\n${OUPUT_MESSAGES.BENEFITS_DETAILS}`);
+    discountList.forEach((e) => {
+      if (e !== 0 && e !== undefined) {
+        Console.print(
+          `${EVENT_NAME[discountList.indexOf(e)]} -${e.toLocaleString()}원`,
+        );
+      } else {
+        Console.print('없음');
+      }
+    });
   },
-
-  // <총혜택 금액> 출력
-  printGift() {
-    Console.print(OUPUT_MESSAGES.TOTAL_BENEFIT_AMOUNT);
+  printDiscountSumAmount(sum) {
+    Console.print(
+      `\n${OUPUT_MESSAGES.TOTAL_BENEFIT_AMOUNT}\n${sum.toLocaleString()}원`,
+    );
   },
-
   // <할인 후 예상 결제 금액> 출력
   printDiscountAmount() {
-    Console.print(OUPUT_MESSAGES.DISCOUNT_AMOUNT);
+    Console.print(`\n${OUPUT_MESSAGES.DISCOUNT_PRICE}\n`);
   },
 
   // <12월 이벤트 배지> 출력
   printEventBadge() {
-    Console.print(OUPUT_MESSAGES.EVENT_BADGE);
+    Console.print(`\n${OUPUT_MESSAGES.EVENT_BADGE}\n`);
   },
 };
 
