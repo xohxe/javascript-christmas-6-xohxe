e3a09f47da53df2b783eb6e752fea7b8c218c329
Sohye <saeyan2477@gmail.com>
2023-11-13 23:58:35 +0900

    feat: EventController 클래스 구현
    - 이벤트 로직 구현 : InputView에서 입력받은 데이터를 Model(Discount 클래스)로 전달하여 할인혜택을 계산하여 Outputview로 전달하여 출력한다.

--- src/controller/EventController.js
+++ src/controller/EventController.js
@@ -2,6 +2,9 @@ import { Console } from '@woowacourse/mission-utils';
 import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
 import Menu from '../model/Menu.js';
+import { ALL_MENU_LIST, EVENT_BADGE } from '../utils/Constants.js';
+import Discount from '../model/Discount.js';
+import Validation from '../validation/Validation.js';
 
 class EventController {
   constructor() {
@@ -14,22 +17,43 @@ class EventController {
     this.ouputView.printIntro();
     const getDate = await this.inputView.readDate();
     const orderMenu = await this.inputView.readMenu();
-    const convertedMenu = this.convertMenu(orderMenu);
-    this.ouputView.printMenu(convertedMenu); // 주문 메뉴 출력
+    this.ouputView.printMenu(orderMenu); // 주문 메뉴 출력
     Console.print(
       `12월 ${getDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
     );
-  }
-  // 주문 금액 계산하기
-  async amountCalculation() {
-    this.ouputView.printOrderAmount();
+    const totalPrice = this.calcTotalPrice(orderMenu);
+    this.ouputView.printOrderPrice(totalPrice);
+    const discount = new Discount();
+    const giftMenu = discount.giftEvent(totalPrice);
+    this.ouputView.printGift(giftMenu);
+    const allDiscount = discount.checkDiscountList(getDate, orderMenu);
+    this.ouputView.printBenefit(allDiscount);
+    const sumDiscount = discount.calcSumDiscount(allDiscount);
+    this.ouputView.printDiscountSumAmount(sumDiscount);
+    const finalPrice = totalPrice - sumDiscount;
+    this.ouputView.printDiscountAmount(finalPrice);
+    const badge = this.getBadge(allDiscount);
+    this.ouputView.printEventBadge(badge[badge.length - 1]);
   }
 
-  // 고객용 목록으로 바꾸기
-  convertMenu(splitedMenu) {
-    return splitedMenu.map((order) => {
-      return new Menu(order.split('-')[0], order.split('-')[1]);
-    });
+  // 할인 전 전체금액 계산
+  calcTotalPrice(menuList) {
+    const orderMenuPrice = menuList.map(
+      (menu) => ALL_MENU_LIST[menu.name] * menu.amount,
+    );
+    const result = orderMenuPrice.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+    return result;
+  }
+  getBadge(discount) {
+    let badge = ['없음'];
+    for (let i = 0; i < 3; i++) {
+      if (discount >= Object.keys(EVENT_BADGE)[i]) {
+        badge.push(Object.values(EVENT_BADGE)[i]);
+      }
+    }
+    return badge;
   }
   // 전체 로직 실행
   async playEvent() {
