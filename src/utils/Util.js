af88526671f3bb33366c89aa1d081e3516ce2cf4
xohxe <saeyan2477@gmail.com>
2023-11-14 23:49:44 +0900

    style: 불필요한 import 제거

--- src/utils/Util.js
+++ src/utils/Util.js
@@ -1,4 +1,7 @@
 import Menu from '../model/Menu.js';
+import { ALL_MENU_LIST } from './Constants.js';
+
+
 class Utils {
   static convertMenu(menus) {
     const splitedMenu = (menus || '').split(',');
@@ -6,6 +9,8 @@ class Utils {
       return new Menu(order.split('-')[0], order.split('-')[1]);
     });
   }
+
+ 
 }
 
 export default Utils;
